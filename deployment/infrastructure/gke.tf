817f321991053a6f360f73e22f29a03fc8d00d5b
Dominik Cebula <dominikcebula@gmail.com>
2024-04-21 19:10:09 +0200

    Adding Helm Chart

--- deployment/infrastructure/gke.tf
+++ deployment/infrastructure/gke.tf
@@ -3,3 +3,19 @@ resource "google_container_cluster" "gke_cluster" {
   location         = var.region
   enable_autopilot = true
 }
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.gke_cluster.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate)
+}
+
+module "todo-service-workload-identity" {
+  source     = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  name       = "todo-service-gke-sa"
+  namespace  = "default"
+  project_id = local.project_id
+  roles      = ["roles/firebase.admin"]
+}
--- deployment/workload/todo-service/templates/_helpers.tpl
+++ deployment/workload/todo-service/templates/_helpers.tpl
@@ -49,3 +49,14 @@ Selector labels
 app.kubernetes.io/name: {{ include "todo-service.name" . }}
 app.kubernetes.io/instance: {{ .Release.Name }}
 {{- end }}
+
+{{/*
+Create the name of the service account to use
+*/}}
+{{- define "todo-service.serviceAccountName" -}}
+{{- if .Values.serviceAccount.create }}
+{{- default (include "todo-service.fullname" .) .Values.serviceAccount.name }}
+{{- else }}
+{{- default "default" .Values.serviceAccount.name }}
+{{- end }}
+{{- end }}
